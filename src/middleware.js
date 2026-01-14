37baf8fec2e9e1190db8388f7b7b2444256dfc33
Lui King Kit <kitluidev@gmail.com>
2026-01-14 23:56:33 +0800

    proxy the resource requests to jsonplaceholder (https://jsonplaceholder.typicode.com)

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,29 @@
+const axios = require('axios');
+
+const proxyToDomain = (targetDomain) => {
+  return async (req, res, next) => {
+    try {
+      const url = `${targetDomain}${req.originalUrl.replace('/api', '')}`;
+
+      const response = await axios({
+        method: req.method,
+        url: url,
+        data: req.body,
+      });
+
+      const total = response.headers['x-total-count'];
+      if (total != undefined) {
+        res.setHeader('x-total-count', total);
+      }
+
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      console.log('proxyToDomain error', error.message);
+      next(error);
+    }
+  };
+};
+
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (error, req, res, next) => {
   res.status(500).json({
@@ -6,5 +32,6 @@ const errorHandler = (error, req, res, next) => {
 };
 
 module.exports = {
+  proxyToDomain,
   errorHandler,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const { proxyToDomain } = require('./middleware');
 
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
-router.use('/', (req, res) => {
-  res.json({
-    message: 'helloworld',
-  });
-});
+router.use('/api', proxyToDomain('https://jsonplaceholder.typicode.com'));
 
 module.exports = router;
